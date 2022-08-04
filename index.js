c143e5626b76a1854baa617bb91aa1c5c292d322
arifvtti <arif.vtti@gmail.com>
2022-08-04 11:24:14 +0600

    Basic Javascript App

---
+++ index.js
@@ -0,0 +1,8 @@
+/* // Basic Javascript App
+const instituteName = prompt("What is your institute name?");
+const roll = prompt("What is your roll number?");
+const semesterFee = Number(prompt("How much your semester fee?"));
+const discountAmount = Number(prompt("Discount %?")) / 100;
+const payableAmount = semesterFee - discountAmount;
+
+alert(`Institute: ${instituteName}\nRoll:${roll}\nSemester Fee: ${semesterFee}\nDiscount: ${discountAmount}\nPayable Amount: ${payableAmount}`); */
